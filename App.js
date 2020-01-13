41034acb2f1a7dbf5afa0f635a8081d2b4f974fd
Klart Dominic <klartdominic@gmail.com>
2020-01-13 09:51:02 +0800

    added loginform branch

--- App.js
+++ App.js
@@ -33,7 +33,7 @@ export default class App extends Component{
           barStyle="light-content" />
         <View style={styles.container}>
           <Routes />
-        </View>
+        </View> 
       </>
     );
   }
